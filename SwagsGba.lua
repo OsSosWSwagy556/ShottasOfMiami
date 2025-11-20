462278dedfacedf38306fe231088d407e31976a5
OsSosWSwagy556 <osamgg347@gmail.com>
2025-11-19 20:29:42 -0800

    Update SwagsGba.lua

--- SwagsGba.lua
+++ SwagsGba.lua
@@ -282,7 +282,7 @@ local RayfieldLibrary = {
 			TabTextColor = Color3.fromRGB(0, 0, 255),
 			SelectedTabTextColor = Color3.fromRGB(50, 50, 50),
 
-			ElementBackground = Color3.fromRGB(25, 25, 25),
+			ElementBackground = Color3.fromRGB(35, 35, 35),
 			ElementBackgroundHover = Color3.fromRGB(40, 40, 40),
 			SecondaryElementBackground = Color3.fromRGB(25, 25, 25),
 			ElementStroke = Color3.fromRGB(50, 50, 50),
