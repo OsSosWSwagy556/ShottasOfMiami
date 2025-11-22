60117b2cac3268dfe5ebaaf8d676fe7ad5ff3384
OsSosWSwagy556 <osamgg347@gmail.com>
2025-11-22 09:39:15 -0800

    Update and rename SwagsGba.lua1 to SwagsGba.lua

--- SwagsGba.lua
+++ SwagsGba.lua
@@ -267,7 +267,7 @@ local RayfieldLibrary = {
 	Flags = {},
 	Theme = {
 		Default = {
-			TextColor = Color3.fromRGB(0, 0, 200),
+			TextColor = Color3.fromRGB(0, 0, 150),
 
 			Background = Color3.fromRGB(20, 20, 20),
 			Topbar = Color3.fromRGB(20, 20, 20),
